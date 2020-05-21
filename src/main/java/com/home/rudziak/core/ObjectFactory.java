9972122776d23ecea16c0aced8f8a1d038ef017e
Artsiom Rudziak <turopvin@mail.ru>
2020-05-21 16:50:26 +0300

    set fields during object creation

--- src/main/java/com/home/rudziak/core/ObjectFactory.java
+++ src/main/java/com/home/rudziak/core/ObjectFactory.java
@@ -3,22 +3,28 @@ package com.home.rudziak.core;
 import com.home.rudziak.core.config.Config;
 import com.home.rudziak.core.config.JavaConfigImpl;
 import com.home.rudziak.usecase.Washer;
-import com.home.rudziak.usecase.impl.HandWasher;
+import com.home.rudziak.usecase.impl.AutomatedWasher;
+import com.home.rudziak.usecase.impl.InjectProperty;
 import lombok.SneakyThrows;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
+
+import static java.util.stream.Collectors.toMap;
 
 public class ObjectFactory {
     private static ObjectFactory ourInstance = new ObjectFactory();
     private Config config;
 
-    public static ObjectFactory getInstance() {
-        return ourInstance;
+    private ObjectFactory() {
+        config = new JavaConfigImpl("com.home", new HashMap<>(Map.of(Washer.class, AutomatedWasher.class)));
     }
 
-    private ObjectFactory() {
-        config = new JavaConfigImpl("com.home", new HashMap<>(Map.of(Washer.class, HandWasher.class)));
+    public static ObjectFactory getInstance() {
+        return ourInstance;
     }
 
     @SneakyThrows
@@ -29,6 +35,20 @@ public class ObjectFactory {
         }
         final T t = implClass.getDeclaredConstructor().newInstance();
 
+        for (Field field : implClass.getDeclaredFields()) {
+            final InjectProperty annotation = field.getAnnotation(InjectProperty.class);
+            final String path = ClassLoader.getSystemClassLoader().getResource("application.properties").getPath();
+            final Map<String, String> propertiesMap = new BufferedReader(new FileReader(path))
+                    .lines()
+                    .map(s -> s.split("="))
+                    .collect(toMap(s1 -> s1[0], s2 -> s2[1]));
+
+            if (annotation != null) {
+                String value = annotation.value().isEmpty() ? propertiesMap.get(field.getName()) : propertiesMap.get(annotation.value());
+                field.setAccessible(true);
+                field.set(t, value);
+            }
+        }
 
 
         return t;
