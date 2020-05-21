78de2055e122cf89ffd9de5782780934ecfc98c1
Artsiom Rudziak <turopvin@mail.ru>
2020-05-21 16:49:29 +0300

    create InjectProperty annotation

---
+++ src/main/java/com/home/rudziak/usecase/impl/InjectProperty.java
@@ -0,0 +1,9 @@
+package com.home.rudziak.usecase.impl;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface InjectProperty {
+    String value() default "";
+}
