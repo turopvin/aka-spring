5a6cd2e35ac088e3e46eaca8d042091bca5677c4
Artsiom Rudziak <turopvin@mail.ru>
2020-05-21 15:55:08 +0300

    finish ObjectFactory

--- src/main/java/com/home/rudziak/core/ObjectFactory.java
+++ src/main/java/com/home/rudziak/core/ObjectFactory.java
@@ -2,10 +2,12 @@ package com.home.rudziak.core;
 
 
 import com.home.rudziak.core.config.Config;
+import com.home.rudziak.core.config.JavaConfigImpl;
+import lombok.SneakyThrows;
 
 public class ObjectFactory {
     private static ObjectFactory ourInstance = new ObjectFactory();
-    private Config config;
+    private Config config = new JavaConfigImpl("com.home");
 
     public static ObjectFactory getInstance() {
         return ourInstance;
@@ -14,12 +16,13 @@ public class ObjectFactory {
     private ObjectFactory() {
     }
 
+    @SneakyThrows
     public <T> T createObject(Class<T> type) {
         Class<? extends T> implClass = type;
         if (type.isInterface()) {
             implClass = config.getImplClass(type);
         }
-        return null;
+        return implClass.getDeclaredConstructor().newInstance();
     }
 
 }
