c8ef63a1f74c9bd80b0143e4c3bc8042b3f7b3b0
Artsiom Rudziak <turopvin@mail.ru>
2020-05-21 17:23:54 +0300

    add method to Config interface

--- src/main/java/com/home/rudziak/core/config/Config.java
+++ src/main/java/com/home/rudziak/core/config/Config.java
@@ -1,6 +1,10 @@
 package com.home.rudziak.core.config;
 
+import java.util.Set;
+
 public interface Config {
 
     <T> Class<? extends T> getImplClass(Class<T> ifc);
+
+    <T> Set<Class<? extends T>> getAllImplClass(Class<T> ifc);
 }
