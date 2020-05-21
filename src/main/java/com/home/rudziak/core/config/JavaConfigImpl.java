c4412474089e70acac05c0288bbff0fda0c84d88
Artsiom Rudziak <turopvin@mail.ru>
2020-05-21 16:19:19 +0300

    configimpl

--- src/main/java/com/home/rudziak/core/config/JavaConfigImpl.java
+++ src/main/java/com/home/rudziak/core/config/JavaConfigImpl.java
@@ -2,22 +2,33 @@ package com.home.rudziak.core.config;
 
 import org.reflections.Reflections;
 
+import java.util.Map;
 import java.util.Set;
 
 public class JavaConfigImpl implements Config {
 
     private Reflections scanner;
+    private Map<Class, Class> ifc2implClass;
 
-    public JavaConfigImpl(String packageToScan) {
+    public JavaConfigImpl(String packageToScan, Map<Class, Class> ifc2implClass) {
         this.scanner = new Reflections(packageToScan);
+        this.ifc2implClass = ifc2implClass;
     }
 
     @Override
     public <T> Class<? extends T> getImplClass(Class<T> ifc) {
+
+        if (ifc2implClass.containsKey(ifc)) {
+            return ifc2implClass.get(ifc);
+        }
+
         final Set<Class<? extends T>> classes = scanner.getSubTypesOf(ifc);
         if (classes.size() != 1) {
             throw new RuntimeException(ifc + " has 0 or more than one impl");
         }
-        return classes.iterator().next();
+        final Class<? extends T> next = classes.iterator().next();
+        ifc2implClass.put(ifc, next);
+        return next;
+
     }
 }
