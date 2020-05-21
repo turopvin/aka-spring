fe1f48d2bc10956840f599400f843cf817a33dbe
Artsiom Rudziak <turopvin@mail.ru>
2020-05-21 16:18:20 +0300

    update objectFactory

--- src/main/java/com/home/rudziak/core/ObjectFactory.java
+++ src/main/java/com/home/rudziak/core/ObjectFactory.java
@@ -1,19 +1,24 @@
 package com.home.rudziak.core;
 
-
 import com.home.rudziak.core.config.Config;
 import com.home.rudziak.core.config.JavaConfigImpl;
+import com.home.rudziak.usecase.Washer;
+import com.home.rudziak.usecase.impl.HandWasher;
 import lombok.SneakyThrows;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ObjectFactory {
     private static ObjectFactory ourInstance = new ObjectFactory();
-    private Config config = new JavaConfigImpl("com.home");
+    private Config config;
 
     public static ObjectFactory getInstance() {
         return ourInstance;
     }
 
     private ObjectFactory() {
+        config = new JavaConfigImpl("com.home", new HashMap<>(Map.of(Washer.class, HandWasher.class)));
     }
 
     @SneakyThrows
@@ -22,7 +27,11 @@ public class ObjectFactory {
         if (type.isInterface()) {
             implClass = config.getImplClass(type);
         }
-        return implClass.getDeclaredConstructor().newInstance();
+        final T t = implClass.getDeclaredConstructor().newInstance();
+
+
+
+        return t;
     }
 
 }
