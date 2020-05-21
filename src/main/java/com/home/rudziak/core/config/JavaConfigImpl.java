907b046453f84d5b2092d26cc2c93690a6ad18bc
Artsiom Rudziak <turopvin@mail.ru>
2020-05-21 15:54:57 +0300

    finish java config

--- src/main/java/com/home/rudziak/core/config/JavaConfigImpl.java
+++ src/main/java/com/home/rudziak/core/config/JavaConfigImpl.java
@@ -1,9 +1,23 @@
 package com.home.rudziak.core.config;
 
+import org.reflections.Reflections;
+
+import java.util.Set;
+
 public class JavaConfigImpl implements Config {
+
+    private Reflections scanner;
+
+    public JavaConfigImpl(String packageToScan) {
+        this.scanner = new Reflections(packageToScan);
+    }
 
     @Override
     public <T> Class<? extends T> getImplClass(Class<T> ifc) {
-        return null;
+        final Set<Class<? extends T>> classes = scanner.getSubTypesOf(ifc);
+        if (classes.size() != 1) {
+            throw new RuntimeException(ifc + " has 0 or more than one impl");
+        }
+        return classes.iterator().next();
     }
 }
