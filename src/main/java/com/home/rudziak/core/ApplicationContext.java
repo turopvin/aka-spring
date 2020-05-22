ef1ee74f9727ad8db1b8739760699084987590b0
Artsiom Rudziak <turopvin@mail.ru>
2020-05-22 11:12:07 +0300

    implement getOject() in ApplicationContext

--- src/main/java/com/home/rudziak/core/ApplicationContext.java
+++ src/main/java/com/home/rudziak/core/ApplicationContext.java
@@ -1,14 +1,33 @@
 package com.home.rudziak.core;
 
+import com.home.rudziak.core.config.Config;
+
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ApplicationContext {
 
-    private ObjectFactory objectFactory;
+    private ObjectFactory factory;
     private Map<Class, Object> container = new ConcurrentHashMap<>();
+    private Config config;
+
+    public ApplicationContext(Config config) {
+        this.config = config;
+    }
 
     public <T> T getObject(Class<T> type) {
-        return null;
+        if (container.containsKey(type)) {
+            return (T) container.get(type);
+        }
+
+        Class<? extends T> implClass = type;
+        if (type.isInterface()) {
+            implClass = config.getImplClass(type);
+        }
+
+        final T t = factory.createObject(implClass);
+        container.put(type, t);
+
+        return t;
     }
 }
