62949f938384e1eff92f0f7ec0306416c8dcfc61
Artsiom Rudziak <turopvin@mail.ru>
2020-05-22 16:27:09 +0300

    prepare connection and table for cars

--- src/main/java/com/home/rudziak/usecase/AutoTechnicalService.java
+++ src/main/java/com/home/rudziak/usecase/AutoTechnicalService.java
@@ -1,9 +1,16 @@
 package com.home.rudziak.usecase;
 
 import com.home.rudziak.core.configurator.annotations.InjectByType;
+import com.home.rudziak.core.configurator.annotations.PostConstruct;
+import com.home.rudziak.core.configurator.annotations.Transactional;
 import com.home.rudziak.usecase.interfaces.Administrator;
 import com.home.rudziak.usecase.interfaces.Washer;
 import com.home.rudziak.usecase.model.Car;
+import lombok.SneakyThrows;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class AutoTechnicalService {
 
@@ -12,6 +19,17 @@ public class AutoTechnicalService {
     @InjectByType
     private Washer washer;
 
+    @PostConstruct
+    @SneakyThrows
+    private void init() {
+        Connection connection = DriverManager
+                .getConnection("jdbc:mysql://localhost:3333/cars", "artsiom", "password");
+        final Statement statement = connection.createStatement();
+        final String initQuery = "CREATE TABLE cars (car_id VARCHAR(9) PRIMARY KEY, model VARCHAR(30))";
+        statement.execute(initQuery);
+    }
+
+    @Transactional
     public void serve(Car car) {
         administrator.register(car);
         service(car);
