5e39faea74789b070f427044b75528278f9cf892
Artsiom Rudziak <turopvin@mail.ru>
2020-05-25 09:18:54 +0300

    update usecase

--- src/main/java/com/home/rudziak/App.java
+++ src/main/java/com/home/rudziak/App.java
@@ -2,7 +2,6 @@ package com.home.rudziak;
 
 import com.home.rudziak.core.ApplicationContext;
 import com.home.rudziak.core.ApplicationRunner;
-import com.home.rudziak.core.ObjectFactory;
 import com.home.rudziak.usecase.AutoTechnicalService;
 import com.home.rudziak.usecase.interfaces.Washer;
 import com.home.rudziak.usecase.interfaces.impl.AutomatedWasher;
@@ -13,14 +12,12 @@ import java.util.Map;
 
 /**
  * Usecase example
- *
  */
-public class App
-{
-    public static void main( String[] args ) {
+public class App {
+    public static void main(String[] args) {
         final ApplicationContext context = ApplicationRunner
                 .run("com.home", new HashMap<>(Map.of(Washer.class, AutomatedWasher.class)));
         AutoTechnicalService service = context.getObject(AutoTechnicalService.class);
-        service.serve(new Car("BMW M5 1994"));
+        service.serve(new Car("BMW M5 1994", "5050 ME-4"));
     }
 }
--- src/main/java/com/home/rudziak/usecase/model/Car.java
+++ src/main/java/com/home/rudziak/usecase/model/Car.java
@@ -9,4 +9,5 @@ import lombok.Setter;
 @Setter
 public class Car {
     private String model;
+    private String serialNumber;
 }
